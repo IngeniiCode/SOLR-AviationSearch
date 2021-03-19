3b07badeb3c5a479eb0b5e0750494a67537a56be
IngeniiCode <ddemartini@ingeniigroup.com>
2021-03-19 10:12:23 -0500

    Added coreRootDiretory to solr.xml to ensure collections/cores automatically loaded

--- aviation_contacts/curl_post_example.sh
+++ aviation_contacts/curl_post_example.sh
@@ -2,6 +2,6 @@
 curl 'http://localhost:8983/solr/aviation_contacts/update?_=1615905561560&commitWithin=1000&overwrite=true&wt=json' \
   -H 'Connection: keep-alive' \
   -H 'Accept: application/json, text/plain, */*' \
-  --data-raw '[{"icao":"AE14C8","callsign":"STGRY00"}]' \
+  --data-raw '[{"icao":"A10000","callsign":"MOODY00","detectedTimestamp":"1995-12-31T23:59:59Z"}]' \
   --compressed \
   --insecure
